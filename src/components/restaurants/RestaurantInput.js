642659a397523e7d96b57d00b1a14d414bb941eb
github username <github email address>
2021-05-09 11:29:16 -0400

    commit tree c8c9b5bf1ec1c808007af84efd15f00d7c636485

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
 
+
 class RestaurantInput extends Component {
+  constructor(props){
+    super(props)
+    this.state =  {
+      text: "",
+    }
+  }
+
+  handleChange = event => {
+    this.setState({
+      text: event.target.value 
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    this.props.addRestaurant(this.state.text)
+    this.setState({
+      text: '',
+    })
+  }
+
   render() {
     return (
       <div>
-        Restaurant Input
+        <form onSubmit={this.handleSubmit}>
+          <p>
+          <label>Name of Restaurant:</label>
+          <input type="text" onChange={this.handleChange} value={this.state.text}/>
+          </p>
+          <input type="submit"/>
+        </form>
+        {this.state.text}
       </div>
     );
   }
 };
 
-export default RestaurantInput;
+//
+
+export default (RestaurantInput);
--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -1,11 +1,17 @@
+
 import React, { Component } from 'react';
 import Review from './Review';
 
 class Reviews extends Component {
   render() {
+    const restReviews = this.props.reviews.filter(review => review.restaurantId === this.props.restaurantId)
+    const listOfReviews = restReviews.map((review, index) => {
+      return <Review key={index} review={review} deleteReview={this.props.deleteReview}/>
+    })
+
     return (
       <ul>
-        Reviews
+        {listOfReviews}
       </ul>
     );
   }
